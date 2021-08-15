f6d04ed0957923fdca8fb6654fbbcc03f2980d38
Mohau <mohaumodisana@gmail.com>
2021-08-15 13:46:52 +0200

    finished making all the buttons work, basically done with the first part of pizza cart

--- pizzaDom.js
+++ pizzaDom.js
@@ -22,6 +22,15 @@ const largeTotal = document.querySelector(".largeTotal");
 //reference total cost
 const totalcosts = document.querySelector(".totalcosts");
 
+//reference buy small pizza
+const buysmall = document.querySelector(".buysmall");
+
+//reference buy medium pizza
+const buymedium = document.querySelector(".buymedium");
+
+//reference buy large pizza
+const buylarge = document.querySelector(".buylarge");
+
 //check out section
 check_out.addEventListener('click', function(){
     check_out.classList.toggle("paybox");
@@ -41,9 +50,9 @@ var totalcartcost=0;
 var smll=0;
 var smallprice=49.00;
 var msnall=49.00;
-radioplus1.addEventListener('click', function(){
-    //radioplus1.classList.toggle("paybox");
-    if (radioplus1){
+
+buysmall.addEventListener('click', function(){
+    if (buysmall){
         smll +=smallprice;
     smallTotal.innerHTML="R" + smll.toFixed(2);
     }
@@ -52,24 +61,45 @@ radioplus1.addEventListener('click', function(){
     totalcosts.innerHTML="R" + totalC.toFixed(2);
 });
 
-radiominus1.addEventListener('click', function(){
-    if (radiominus1 && smll>0){
-    //radiominus1.classList.toggle("paybox");
-    smll -=msnall;
-    smallTotal.innerHTML="R" + smll.toFixed(2);
-    }
+    radioplus1.addEventListener('click', function(){
+        //radioplus1.classList.toggle("paybox");
+        if (radioplus1){
+            smll +=smallprice;
+        smallTotal.innerHTML="R" + smll.toFixed(2);
+        }
 
         totalC=smll+medium+large;
-   totalcosts.innerHTML="R" + totalC.toFixed(2);
-});
+        totalcosts.innerHTML="R" + totalC.toFixed(2);
+    });
 
-var totalC=0.00;
+    radiominus1.addEventListener('click', function(){
+        if (radiominus1 && smll>0){
+        //radiominus1.classList.toggle("paybox");
+        smll -=msnall;
+        smallTotal.innerHTML="R" + smll.toFixed(2);
+        }
+
+            totalC=smll+medium+large;
+        totalcosts.innerHTML="R" + totalC.toFixed(2);
+    });
+
             //MEDIUM PIZZA CART
 
 //add/subtr for medium pizza
 var medium=0;
 var addmedium=89.00;
 var minusmedium=89.00;
+
+buymedium.addEventListener('click', function(){
+    //radioplus2.classList.toggle("paybox");
+    if(buymedium){
+    medium +=addmedium;
+    mediumTotal.innerHTML="R" + medium.toFixed(2);
+    }
+    totalC=smll+medium+large;
+    totalcosts.innerHTML="R" + totalC.toFixed(2);
+});
+
 radioplus2.addEventListener('click', function(){
     //radioplus2.classList.toggle("paybox");
     if(radioplus2){
@@ -95,6 +125,17 @@ radiominus2.addEventListener('click', function(){
 //add/subtr for large pizza
 var large=0;
 var addlarge=129.00;
+
+buylarge.addEventListener('click', function(){
+    //radioplus3.classList.toggle("paybox");
+    if(buylarge){
+        large +=addlarge;
+        largeTotal.innerHTML="R" + large.toFixed(2);
+    }
+    totalC=smll+medium+large;
+    totalcosts.innerHTML="R" + totalC.toFixed(2);
+});
+
 radioplus3.addEventListener('click', function(){
     //radioplus3.classList.toggle("paybox");
     if(radioplus3){
